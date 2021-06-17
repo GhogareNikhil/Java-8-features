f2135c010f80a346e703e97e52236756b3928548
GhogareNikhil <ghogarenikhil00@gmail.com>
2021-06-17 16:34:45 +0530

    changes in file

--- src/StreamApi/FindAllEvenNumberBetween1To5.java
+++ src/StreamApi/FindAllEvenNumberBetween1To5.java
@@ -12,7 +12,7 @@ public class FindAllEvenNumberBetween1To5 {
 		
 		// OR
 		
-		System.out.println("*****");
+		System.out.println("**###***");
 		IntStream.of(1, 2, 3, 4, 5, 6).filter(n-> n % 2 == 0).forEach(System.out::println);
 	}
 
