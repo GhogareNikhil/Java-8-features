45fb44c321e7328ab98d0a07f0d3ddf7ade89267
Nikhil Ghogare <ghogarenikhil00@gmail.com.com>
2023-06-04 19:41:49 +0530

    commit by aksh

--- src/gitPractice/Test.java
+++ src/gitPractice/Test.java
@@ -2,6 +2,6 @@ package gitPractice;
 
 public class Test {
    public static void main(String[] args) {
-	System.out.println("Hello Nikhil");
+	System.out.println("Hello Nikhil How Are You");
   }
 }
